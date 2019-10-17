c4af5bd113d412365e5d33b5067383122070e931
cdfortes <cdfortes@gmail.com>
2019-10-17 21:44:23 -0100

    scrapping to a file csv

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const Table = require('cli-table');
+const fs = require('fs');
+
+
+const writeStream = fs.createWriteStream('farmacias.csv');
+
+
+writeStream.write(`Data,Farmacia,Endereço,Telefone,Fax \n`);
+
 
 let table = new Table({
     head: ['data', 'Nome', 'zona', 'phone', 'pone2'],
@@ -15,20 +23,19 @@ const options = {
 
 rp(options)
     .then(function ($) {
-        var p = 0;
-        
+
         process.stdout.write('loading');
         $('.table-farmacias-servico tbody tr', '#quicktabs-tabpage-se_farmacias_servico-2').each((i, el) => {
             const arr = [];
             $('td', el).each((j, ol) => {
                 arr.push($(ol).text());
                 process.stdout.write(`.`);
-                
             })
             table.push([arr[0], arr[1], arr[2], arr[3], arr[4]]);
+            writeStream.write(`${arr[0]}, ${arr[1]}, ${arr[2]}, ${arr[3]}, ${arr[4]} \n`)
         });
-        
         printData();
+
     })
     .catch(function (err) {
         console.log(err);
