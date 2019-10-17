1e76c83d7d2d9b200aaa0d1b3b635c06d8434fc3
cdfortes <cdfortes@gmail.com>
2019-10-16 23:46:18 -0100

    emprofac scrapping

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const Table = require('cli-table');
+
+const options = {
+    url: `http://www.emprofac.cv/informacoes-uteis/farmacia-servico?qt-se_farmacias_servico=2`,
+    transform: body => cheerio.load(body)
+};
+
+rp(options)
+    .then(function ($) {
+        console.log($('.table-farmacias-servico', '#quicktabs-tabpage-se_farmacias_servico-2').html());
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
